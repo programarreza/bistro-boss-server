0348439bc98a3f69d90f0b8485d8e866d5f9acd0
programarreza <programarreza.com>
2023-11-21 21:38:46 +0600

    get the order information

--- index.js
+++ index.js
@@ -229,6 +229,86 @@ async function run() {
       res.send({ paymentResult, deleteResult });
     });
 
+    // stats or analytics
+    app.get("/admin-stats",verifyToken, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // this is not the best way
+      // const payments = await paymentCollection.find().toArray()
+      // const revenue  = payments.reduce((total, payment) => total + payment.price, 0)
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue,
+      });
+    });
+
+    // order status
+    /**
+     * NON-Efficient Way
+     * ------------------
+     * 1. load all the payments
+     * 2. for every menuItems (which is an array), go find the item in menu collection
+     * 3. for every item in the menu collection that you found from an payment entry (document)
+     * 4.
+     */
+
+    // using aggregate pipeline
+    app.get("/order-stats",verifyToken, verifyAdmin, async (req, res) => {
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $unwind: "$menuItemsIds",
+          },
+          {
+            $lookup: {
+              from: "menu",
+              localField: "menuItemsIds",
+              foreignField: "_id",
+              as: "menuItems",
+            },
+          },
+          {
+            $unwind: "$menuItems",
+          },
+          {
+            $group: {
+              _id: "$menuItems.category",
+              quantity: { $sum: 1 },
+              revenue: { $sum: "$menuItems.price" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              category: '$_id', 
+              quantity: '$quantity',
+              revenue: '$revenue'
+            }
+          }
+        ])
+        .toArray();
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
